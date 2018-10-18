8009eac1f93413ba426013d83dca27dd6604ad02
bryanjv <bryan.jeldes.12@sansano.usm.cl>
2018-10-18 15:33:33 -0300

    Multiplicacion Agregada

--- math_operations.js
+++ math_operations.js
@@ -22,6 +22,25 @@ function add (query, response) {
   response.end(JSON.stringify({result:suma}))
 }
 
+function mult (query, response) {
+  var mult = 1
+  for (var propName in query) {
+      var splitted = query[propName]
+      if(isNumeric(splitted)) {
+        //Ok, it's a number
+        var number = Number(splitted)
+        mult *= number
+      } else {
+        //It's not a number, so we show an error message and sends to the client
+        response.writeHead(400, {'Content-Type': 'application/json'})
+        response.end(JSON.stringify({result:'error, el input debe ser numerico'}))
+      }
+  }
+  //If everithing was OK, we send code 200 response, using json format
+  response.writeHead(200, {'Content-Type': 'application/json'})
+  response.end(JSON.stringify({result:mult}))
+}
+
 /**
  ** isNumeric returns true if input is a number. This can be done using the oppsite behaviour from isNaN function
  **/
@@ -32,3 +51,4 @@ function isNumeric(num) {
 
 //In this line we export the functions to be used in other javascript files. We must import this module with 'require' signature
 module.exports.add = add
+module.exports.mult = mult
--- node_example.js
+++ node_example.js
@@ -8,6 +8,10 @@ var server = http.createServer(function (request, response) {
   if(parsedUrl.pathname === '/suma') {
     math_ops.add(parsedUrl.query, response)
   }
+
+  if(parsedUrl.pathname === '/multiplicacion') {
+    math_ops.mult(parsedUrl.query, response)
+  }
 })
 
 server.listen(8080)
